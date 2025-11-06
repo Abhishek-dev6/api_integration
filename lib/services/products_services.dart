0fe8e1d0b2487b01beb564e248e303657d9abe8f
Abhishek-dev6 <kbabhishek223@gmail.com>
2025-11-06 11:12:54 +0530

    created Services folder and cretaed a new service file and added PUT method

---
+++ lib/services/products_services.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+Future<void> updateproduct(int id,Map <String,dynamic> updateData) async{
+  final url=Uri.parse('https://api.restful-api.dev/objects/$id');
+  final response=await http.put(
+    url,headers:{'COntent-Type':'application/json'},
+    body:jsonEncode(updateData),
+  );
+  if(response.statusCode==200){
+    print("product updated successfully");
+    print(response.body);
+  }else{
+    print("failed to update");
+  }
+}
